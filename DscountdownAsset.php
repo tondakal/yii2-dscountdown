dea86490709146283327df9b9f7e418e4ff20c23
tonda <tonda@localhost.localdomain>
2015-04-20 09:58:56 +0200

    fixed asset

--- DscountdownAsset.php
+++ DscountdownAsset.php
@@ -6,7 +6,7 @@ use yii\web\AssetBundle;
 
 class DscountdownAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/dscountdown';
+    public $sourcePath = '@bower/dscountdown-new';
     public $css = ['dscountdown.css'];
     public $js = ['dscountdown.js'];
     public $depends = [
